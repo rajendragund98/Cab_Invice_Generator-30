34318a2eadc47f75d02c4393717c1ee8755f9275
RajendraGund <rajendragund2015@gmail.com>
2021-09-28 03:17:53 +0530

    Added : Welcome message for the user

---
+++ src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.assignment;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome in cab invice generator program !!!!");
+    }
+}
