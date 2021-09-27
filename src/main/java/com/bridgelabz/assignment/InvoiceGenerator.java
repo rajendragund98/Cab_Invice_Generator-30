cea50bd9c1bb9b641e783d4c7cc17763c02acf37
RajendraGund <rajendragund2015@gmail.com>
2021-09-28 03:43:47 +0530

    Added : Program for the returns total rides, total fare and avg fare for each ride

--- src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.assignment;
 
+
 public class InvoiceGenerator {
     //constant variables
     private static final double MINIMUM_COST_PER_KM = 10.0;
@@ -9,17 +10,24 @@ public class InvoiceGenerator {
     //calculate the total fare
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
-        if (totalFare < MINIMUM_FARE)
-            return MINIMUM_FARE;
-        else
-            return totalFare;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
+
     //calculating Fare for Multiple rides
-    public double calculateFare(Ride[] rides){
+    public double calculateFare(Ride[] rides) {
         double totalFare = 0;
-        for (Ride ride : rides){
+        for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return totalFare;
+    }
+
+    //calculating totalFare, average Fare for multiple rides
+    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
