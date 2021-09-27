4ba2871b868c03e642e5522ea94960bab9b490f9
RajendraGund <rajendragund2015@gmail.com>
2021-09-28 03:51:30 +0530

    Added : Program for the get the list from the ride repository

--- src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
@@ -6,6 +6,7 @@ public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
     private static final int MINIMUM_FARE = 5;
+    private RideRepository rideRepository;
 
     //calculate the total fare
     public double calculateFare(double distance, int time) {
@@ -13,21 +14,40 @@ public class InvoiceGenerator {
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    //calculating Fare for Multiple rides
-    public double calculateFare(Ride[] rides) {
-        double totalFare = 0;
-        for (Ride ride : rides) {
-            totalFare += this.calculateFare(ride.distance, ride.time);
-        }
-        return totalFare;
+    public InvoiceGenerator() {
+        this.rideRepository = new RideRepository();
     }
 
-    //calculating totalFare, average Fare for multiple rides
-    public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+    //calculating Fare for Multiple rides
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return new InvoiceSummary(rides.length, totalFare);
     }
+
+    public InvoiceSummary calculateFare(Ride[] rides, String type) {
+        double totalFare = 0;
+        if (type == "premium") {
+            for (Ride ride : rides) {
+                totalFare += this.calculateFare(ride.distance, ride.time);
+            }
+            return new InvoiceSummary(rides.length, totalFare);
+        }
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRide(userId, rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId), "normal");
+    }
+
 }
