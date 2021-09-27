15d0fdf6028e035eab782e70fd63a1eedc991ec4
RajendraGund <rajendragund2015@gmail.com>
2021-09-28 03:27:01 +0530

    Added : Program for the Calculate the fare for given distance and time

--- src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/assignment/InvoiceGenerator.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.assignment;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome in cab invice generator program !!!!");
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final int MINIMUM_FARE = 5;
+
+    public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        else
+            return totalFare;
     }
 }
